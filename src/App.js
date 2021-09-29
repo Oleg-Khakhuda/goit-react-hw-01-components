8df85fe876b40f148cdd83a96b88c00af5435f5e
Oleg-Khakhuda <buddy2317@gmail.com>
2021-09-29 22:57:09 +0300

    Добавляет task-1,2

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+import './App.css';
+import user from './components/Profile/user.json';
+import Profile from './components/Profile/Profile';
+import statisticalData from './components/Statistics/statistical-data.json'
+import Statistics from './components/Statistics/Statistics';
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <Statistics stats={statisticalData} />
+      
+    </div>
+    )
+}
